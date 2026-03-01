1d82b4ee60120d893c956b8023655af5ea9d0ea6
Jeffrey Zhuang <jeffreyzhuangyt@gmail.com>
2026-02-28 23:59:57 -0500

    Add serial communication

--- hil_controller.py
+++ hil_controller.py
@@ -7,4 +7,15 @@ class HILController(Controller):
         self.ser = serial.Serial(port, baudrate)
 
     def update(self, missile, target):
+        s = "0.0,0.0,0.0\r"
+        self.ser.write(s.encode('utf-8'))
+
+        line = self.ser.readline()
+        try:
+            line = line.decode("utf-8").strip().split(",")
+            line = [float(v) for v in line]
+            print(line)
+        except ValueError:
+            print("Parse error:", line)
+
         return 0, 0
