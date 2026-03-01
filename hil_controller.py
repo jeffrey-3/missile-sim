090a0ac347c1bff88fbec7c007ca7c5a9134c650
Jeffrey Zhuang <jeffreyzhuangyt@gmail.com>
2026-03-01 00:19:52 -0500

    Convert pulse to servo angle

--- hil_controller.py
+++ hil_controller.py
@@ -5,6 +5,8 @@ from controller import Controller
 class HILController(Controller):
     def __init__(self, port: str, baudrate: int):
         self.ser = serial.Serial(port, baudrate)
+        self.servo_y_angle = 0.0
+        self.servo_z_angle = 0.0
 
     def update(self, missile, target):
         s = (f"{missile.omega[0]:.3f},{missile.omega[1]:.3f},"
@@ -17,7 +19,19 @@ class HILController(Controller):
             line = line.decode("utf-8").strip().split(",")
             line = [float(v) for v in line]
             print(line)
+
+            max_pulse = 2100.0
+            min_pulse = 900.0
+            middle_pulse = (max_pulse + min_pulse) / 2.0
+            pulse_range = max_pulse - min_pulse
+            deflection_range = np.rad(10.0)
+            line[0] = np.clamp(line[0], min_pulse, max_pulse)
+            line[1] = np.clamp(line[1], min_pulse, max_pulse)
+            servo_y_angle = (line[0] - middle_pulse) / pulse_range
+                * deflection_range
+            servo_z_angle = (line[1] - middle_pulse) / pulse_range
+                * deflection_range
         except ValueError:
             print("Parse error:", line)
 
-        return 0, 0
+        return servo_y_angle, servo_z_angle
