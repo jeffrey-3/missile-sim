c9397873ba83b0cccb19299ecb5bacd4a18c0a1f
Jeffrey Zhuang <jeffreyzhuangyt@gmail.com>
2026-03-01 01:23:55 -0500

    Use E16 thrust curve

--- motor.py
+++ motor.py
@@ -2,8 +2,9 @@ import numpy as np
 
 class Motor:
     def __init__(self):
-        self.lut_time = np.array([0.0, 1.0, 2.0, 3.0])
-        self.lut_thrust = np.array([30.0, 50.0, 10.0, 0.0])
+        # Estes E16 Thrust Curve
+        self.lut_time = np.array([0.0, 0.2, 0.5, 1.0, 2.0, 2.1])
+        self.lut_thrust = np.array([0.0, 5.0, 26.0, 16.0, 16.0, 0.0])
 
     def get_thrust_body(self, time_elapsed):
         thrust = np.interp(time_elapsed, self.lut_time, self.lut_thrust)
